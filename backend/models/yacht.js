7ec6d728154c0809fe0324dd1302063045b65555
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-19 17:55:33 +0300

    im lost god help me

--- backend/models/yacht.js
+++ backend/models/yacht.js
@@ -2,15 +2,21 @@ import mongoose from 'mongoose';
 
 const yachtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  type: { type: String, required: true },
+  type: {
+    tr: { type: String, required: true },
+    en: { type: String, required: true },
+  },
   length: { type: Number, required: true },
   people: { type: Number, required: true },
   cabin: { type: Number, required: true },
   location: { type: String, required: true },
   images: { type: Number, required: true },
-  features: [{ type: String }],
+  features: {
+    tr: { type: [String] },
+    en: { type: [String] },
+  },
 });
 
-const Yacht = mongoose.model('Yacht', yachtSchema);
+const Yacht = mongoose.model('Yacht', yachtSchema, 'yachts');
 
 export default Yacht;
