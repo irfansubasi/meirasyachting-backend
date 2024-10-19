84457daa38cc9125c9344ccd934f873fd6f1c1a7
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-19 17:15:02 +0300

    fix gets

--- backend/server.js
+++ backend/server.js
@@ -33,57 +33,11 @@ app.get('/', (req, res) => {
   res.send('API çalışıyor!');
 });
 
-app.post('/yachts', async (req, res) => {
-  const { name, type, length, people, cabin, location, features, images } =
-    req.body;
-
-  const yeniYat = new Yacht({
-    name: {
-      tr: name.tr,
-      en: name.en,
-    },
-    type: {
-      tr: type.tr,
-      en: type.en,
-    },
-    length,
-    people,
-    cabin,
-    location: {
-      tr: location.tr,
-      en: location.en,
-    },
-    features: {
-      tr: features.tr,
-      en: features.en,
-    },
-    images,
-  });
-
-  try {
-    await yeniYat.save();
-    res
-      .status(201)
-      .json({ message: 'Yat başarıyla kaydedildi!', yat: yeniYat });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: 'Yat kaydetme işlemi başarısız oldu.',
-      details: error.message,
-    });
-  }
+app.get('/yachts', (req, res) => {
+  res.status(200).json({ message: 'Bu endpoint veri döndürmüyor.' });
 });
 
-app.get('/yachts', async (req, res) => {
-  try {
-    const yatlar = await Yacht.find();
-    res.status(200).json(yatlar);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Yatları alma işlemi başarısız oldu.' });
-  }
-});
-
+// Türkçe yat verileri
 app.get('/yachts/TR', async (req, res) => {
   try {
     const yatlar = await Yacht.find();
@@ -196,30 +150,6 @@ app.post('/send-email', (req, res) => {
     } else {
       return res.status(200).json({ message: 'Email sent successfully!' });
     }
-  });
-});
-
-app.get('/api/yacht-images/:id', (req, res) => {
-  const yachtId = req.params.id;
-  const imageDirectory = path.join(
-    __dirname,
-    '..',
-    'public',
-    'assets',
-    'yachtspics',
-    yachtId
-  );
-
-  fs.readdir(imageDirectory, (err, files) => {
-    if (err) {
-      return res
-        .status(500)
-        .json({ error: 'Directory not found or an error occurred.' });
-    }
-
-    // Resim dosyalarının tam yolunu oluştur
-    const images = files.map((file) => `/assets/yachtspics/${yachtId}/${file}`);
-    res.json({ images });
   });
 });
 
